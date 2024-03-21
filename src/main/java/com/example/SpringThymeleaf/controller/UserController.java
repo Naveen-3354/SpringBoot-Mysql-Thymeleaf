a666da2d2fcd9e9d299d9e255097732f8d15bf81
Naveen-3354 <welbe1709@gmail.com>
2024-03-21 18:53:49 +0530

    second commit from local

--- src/main/java/com/example/SpringThymeleaf/controller/UserController.java
+++ src/main/java/com/example/SpringThymeleaf/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.example.SpringThymeleaf.controller;
 
 import com.example.SpringThymeleaf.model.UserDetails;
+import com.example.SpringThymeleaf.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
+
+    @Autowired
+    private UserService userService;
 
     @GetMapping("/")
     public String Login(Model model){
@@ -26,6 +31,7 @@ public class UserController {
 
     @PostMapping("/save")
     public String save(@ModelAttribute("UserDetails") UserDetails userDetails,Model model){
+        userService.save(userDetails);
         return "redirect:/";
     }
 }
--- src/main/java/com/example/SpringThymeleaf/service/UserService.java
+++ src/main/java/com/example/SpringThymeleaf/service/UserService.java
@@ -1,6 +1,7 @@
 package com.example.SpringThymeleaf.service;
 
 import com.example.SpringThymeleaf.dao.UserRepository;
+import com.example.SpringThymeleaf.model.UserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,5 +11,7 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-
+    public void save(UserDetails user) {
+        userRepository.save(user);
+    }
 }
